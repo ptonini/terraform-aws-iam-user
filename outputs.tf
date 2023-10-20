1308eda3fc901da5395c41162ebaea4bed662346
ptonini <pedro.tonini@hotmail.com>
2023-10-20 12:59:02 -0300

    refactor!: removed console access support

--- outputs.tf
+++ outputs.tf
@@ -7,20 +7,16 @@ output "arn" {
 }
 
 output "access_key_id" {
-  value = try(aws_iam_access_key.this[0].id, null)
+  value     = try(aws_iam_access_key.this[0].id, null)
   sensitive = true
 }
 
 output "secret_access_key" {
-  value = try(aws_iam_access_key.this[0].secret, null)
+  value     = try(aws_iam_access_key.this[0].secret, null)
   sensitive = true
 }
 
 output "ses_secret_access_key" {
-  value = try(aws_iam_access_key.this[0].ses_smtp_password_v4, null)
+  value     = try(aws_iam_access_key.this[0].ses_smtp_password_v4, null)
   sensitive = true
-}
-
-output "encrypted_password" {
-  value = var.console ? aws_iam_user_login_profile.this.0.encrypted_password : null
 }
